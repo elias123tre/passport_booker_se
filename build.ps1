19a1b00448b649cc50eb87ed16c297e6b6711d82
Elias <elias@floreteng.se>
2022-03-24 00:57:51 +0100

    Windows build script

---
+++ build.ps1
@@ -0,0 +1,10 @@
+# Make sure you have a virtualenv in the folder `venv` with playwright and pyinstaller installed
+# Steps to do the above:
+# 1. `python -m venv venv`
+# 2. `.\venv\Scripts\Activate.ps1`
+# 3. `pip install playwright pyinstaller`
+# 4. Run this file to build a packaged binary
+
+$env:PLAYWRIGHT_BROWSERS_PATH="0"
+& .\venv\Scripts\playwright.exe install chromium
+& .\venv\Scripts\pyinstaller --clean --onefile --noconsole main.py
